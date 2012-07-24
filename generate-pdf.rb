119d24e8b4d34208c8a156e48f325d4b63f3aef7
David Nelson <davidmichael.nelson@gmail.com>
2012-07-24 01:17:20 -0300

    Script to create an PDF with images from a folder

---
+++ generate-pdf.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'prawn'
+
+abort "Usage: generate-pdf <pdf_file_name> <img-folder>." unless ARGV.size == 2
+
+pdf = ARGV[0]
+folder = ARGV[1]
+
+Prawn::Document.generate(pdf, :page_size => [820, 620], :margin => 10, :layout => :portrait) do
+  images = Dir.glob("#{folder}/*.{png,jpg,jpeg}", File::FNM_CASEFOLD).sort
+  images.each do |image_file|
+    start_new_page if image_file != images.first 
+    text image_file.scan(/\d{4}_\d{2}_\d{2}/).first
+    image image_file, :fit => [800, 570] 
+  end
+end
